b5c2178ac391bf19a9ea56087295c92a4e2a10b0
Muhammad Ikram bin Azhar <lalatputih22@gmail.com>
2024-08-13 23:55:01 +0800

    rmeove crd

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const accountSid = 'AC39f596e11de7412757a1005122b27fd3';
-const authToken = '0174e01095199637b57b146b3f838cf1';
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 console.log("kambing");
 client.messages
     .create({
         body: 'Your appointment is coming up on July 21 at 3PM qweq we',
-        from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+60175845874'
+        from: `whatsapp:+${process.env.TWILIO_NUMBER}`,
+        to: `01234567889`
     })
     .then(message => console.log(message.sid))
     console.log("ayam");
